16664953230ab4f194f08e718dee7b0183ebc6f3
Tatyana <172027675+TatyanaVTV@users.noreply.github.com>
2024-09-06 15:51:26 +0300

    Two functions were added (fix).

--- src/test/java/ru/vtvhw/maven/MavenCircleTest.java
+++ src/test/java/ru/vtvhw/maven/MavenCircleTest.java
@@ -7,8 +7,10 @@ import static org.junit.Assert.assertEquals;
 public class MavenCircleTest {
     @Test
     public void createMavenCircle() {
-        MavenCircle mavenCircle = new MavenCircle(4);
-        System.out.println(mavenCircle);
-        assertEquals(4, mavenCircle.getRadius());
+        MavenCircle mavenCircle = new MavenCircle(3);
+        System.out.printf("Maven Circle: %s%n", mavenCircle);
+        System.out.printf("Circle's area = %f%n", mavenCircle.getArea());
+        System.out.printf("Circle's length = %f%n", mavenCircle.getLength());
+        assertEquals(3, mavenCircle.getRadius());
     }
 }
